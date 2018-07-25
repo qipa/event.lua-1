90277f9c81447e5fceb9cf3f82aaae48da04f8d4
4s <4s@localhost.localdomain>
2018-07-25 12:37:58 +0800

    update

--- shell/vim_install.sh
+++ shell/vim_install.sh
@@ -1,43 +1,53 @@
 #!/bin/bash
 
-PATH='pwd'
-VIMRC=${PATH}/vimrc
+CURPATH='pwd'
+VIMRC=${CURPATH}/vimrc
 
 cd ~
-mkdir vim_asset
-cd ./vim/vim_asset
+if [ ! -d ./vim_asset ];then
+	mkdir vim_asset
+fi
+cd ./vim_asset
 
-git clone https://github.com/vim/vim.git
+if [ ! -d ./vim ];then
+	git clone https://github.com/vim/vim.git
+fi
 
-git clone https://github.com/ggreer/the_silver_searcher.git
+if [ ! -d ./the_silver_searcher ];then
+	git clone https://github.com/ggreer/the_silver_searcher.git
+fi
 
-git clone https://github.com/universal-ctags/ctags.git
+if [ ! -d ./ctags ];then 
+	git clone https://github.com/universal-ctags/ctags.git
+fi
 
-wget https://www.python.org/ftp/python/2.7.5/Python-2.7.5.tgz
+if [ ! -f ./Python-2.7.5.tgz ];then
+	wget https://www.python.org/ftp/python/2.7.5/Python-2.7.5.tgz
+	tar zxvf Python-2.7.5.tgz
+	cd Python-2.7.5.tgz && ./configure && make
+fi
 
 cd vim
 ./configure  --enable-pythoninterp=yes --with-python-config-dir=/usr/lib/python2.7/config
-make
-sudo make install
-
+sudo make && sudo make install
 cp /usr/local/bin/vim /usr/bin/vim
 
-if [ ! -d "~/.vim" ];then
+if [ ! -d ~/.vim ];then
 	mkdir ~/.vim
 fi
 
-if [ ! -d "~/.vim/autoload" ];then
+if [ ! -d ~/.vim/autoload ];then
 	mkdir ~/.vim/autoload
 fi
 
 cd ~/.vim/autoload
 
-
-wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-
-if [ -d "~/.vimrc" ];then
-	rm ~/.vimrc
+if [ ! -d ./plug.vim ];then
+	wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-cp ${VIMRC} ~/.vimrc
+
+if [ ! -f ~/.vimrc ];then
+	cp ${VIMRC} ~/.vimrc
+fi
+
