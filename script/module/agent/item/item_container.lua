d8f2a9d3348288abd929d9c0fd657116960b348a
2109 <cai32109@gmail.com>
2018-09-03 19:50:51 +0800

    bug

--- script/module/agent/item/item_container.lua
+++ script/module/agent/item/item_container.lua
@@ -3,18 +3,18 @@ local database_collection = import "module.database_collection"
 local item_factory = import "module.item.item_factory"
 
 
-cls_item_container = database_collection.cls_collection_set:inherit("item_container")
+cItemMgr = database_collection.cls_collection_set:inherit("item_mgr")
 
 function __init__(self)
 	
 end
 
 
-function cls_item_container:create()
+function cItemMgr:create()
 
 end
 
-function cls_item_container:init()
+function cItemMgr:init()
 	self.helper = {}
 	for uid,item in pairs(self.slots) do
 		local info = self.helper[item.cid]
@@ -26,11 +26,11 @@ function cls_item_container:init()
 	end
 end
 
-function cls_item_container:destroy()
+function cItemMgr:destroy()
 
 end
 
-local function add_item(self,item)
+local function _addItem(self,item)
 	self.slots[item.uid] = item
 	local info = self.helper[item.cid]
 	if not info then
@@ -40,28 +40,29 @@ local function add_item(self,item)
 	info[item.uid] = true
 end
 
-local function del_item(self,item)
+local function _delItem(self,item)
 	self.slots[item.uid] = nil
-	helper_info[item.uid] = nil
+	local helperInfo = self.helper[item.cid]
+	helperInfo[item.uid] = nil
 	item:destroy()
 end
 
-function cls_item_container:insertItemByCid(cid,amount)
+function cItemMgr:insertItemByCid(cid,amount)
 
-	local item_conf = config.item[cid]
+	local itemConf = config.item[cid]
 
 	local left = amount
-	local uid_list = self.helper[cid]
-	for uid in pairs(uid_list) do
-		local old_item = self.slots[uid]
-		if item_conf.overlap > old_item.amount then
-			local space = item_conf.overlap - old_item.amount
+	local helperInfo = self.helper[cid]
+	for uid in pairs(helperInfo) do
+		local oItem = self.slots[uid]
+		if itemConf.overlap > oItem.amount then
+			local space = itemConf.overlap - oItem.amount
 			if left < space then
-				old_item.amount = old_item.amount + left
+				oItem.amount = oItem.amount + left
 				left = 0
 				break
 			else
-				old_item.amount = item_conf.overlap
+				oItem.amount = itemConf.overlap
 				left = left - space
 			end
 		end
@@ -71,59 +72,65 @@ function cls_item_container:insertItemByCid(cid,amount)
 		local items = item_factory:create_item(cid,left)
 
 		for _,item in pairs(items) do
-			add_item(self,item)
+			_addItem(self,item)
 		end
 	end
 end
 
-function cls_item_container:insert_item(item)
-	local item_conf = config.item[item.cid]
-	if item_conf.overlap > 1 then
+function cItemMgr:insertItem(item)
+	local itemConf = config.item[item.cid]
+	if itemConf.overlap > 1 then
 		self:insertItemByCid(item.cid,item.amount)
-		item:destroy()
+		item:release()
 		return
 	end
 
-	add_item(self,item)
+	_addItem(self,item)
 end
 
-function cls_item_container:delete_item(item)
-	return self:delete_item_by_uid(item.uid,item.amount)
+function cItemMgr:deleteItem(item)
+	return self:deleteItemByUid(item.uid,item.amount)
 end
 
-function cls_item_container:delete_item_by_cid(cid,amount)
-	if not self:item_enough(cid,amount) then
+function cItemMgr:deleteItemByCid(cid,amount)
+	if not self:itemEnough(cid,amount) then
 		return false
 	end
 
-	local helper_info = self.helper[cid]
+	local helperInfo = self.helper[cid]
 
 	local left = amount
 
-	for uid in pairs(helper_info) do
+	for uid in pairs(helperInfo) do
 		if left == 0 then
 			break
 		end
 
-		local old_item = self.slots[uid]
-		if old_item.amount > left then
-			old_item.amount = old_item.amount - left
+		local oItem = self.slots[uid]
+		if oItem.amount > left then
+			oItem.amount = oItem.amount - left
 			left = 0
 		else
-			left = left - old_item.amount
-			del_item(self,old_item)
+			left = left - oItem.amount
+			_delItem(self,oItem)
 		end
 	end
 end
 
-function cls_item_container:delete_item_by_uid(uid,amount)
-
+function cItemMgr:deleteItemByUid(uid,amount)
+	local item = self.slots[uid]
+	if item.amount > amount then
+		item.amount = item.amount - amount
+		self:dirty_field(uid)
+	else
+		_delItem(self,item)
+	end
 end
 
-function cls_item_container:item_enough(cid,amount)
-	local helper_info = self.helper[cid]
+function cItemMgr:itemEnough(cid,amount)
+	local helperInfo = self.helper[cid]
 	local total = 0
-	for uid in pairs(helper_info) do
+	for uid in pairs(helperInfo) do
 		local item = self.slots[uid]
 		total = total + item.amount
 	end
