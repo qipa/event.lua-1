de29647ffe4d5f4e3492a59288a57cf982a0dc2d
2109 <cai32109@gmail.com>
2018-09-06 10:48:29 +0800

    bug

--- script/module/id_builder.lua
+++ script/module/id_builder.lua
@@ -2,114 +2,82 @@ local persistence = require "persistence"
 local model = require "model"
 require "lfs"
 
-local unique_offset = {
+local kPROCESS_OFFSET = 100
+local kSERVER_OFFSET = 10000
+local kMASK_OFFSET = 10
+local kID_STEP = 100
+
+local eUNIQUE_MASK = {
 	user = 1,
 	item = 2,
 	scene = 3,
 }
 
-local tmp_offset = {
+
+local eTMP_MASK = {
 	monster = 1
 }
 
-local dist_offset = 100
-local id_section = 100
-
--- function init(self,dist_id)
--- 	if dist_id >= dist_offset then
--- 		print(string.format("error dist id:%d",dist_id))
--- 		os.exit(1)
--- 	end
-	
--- 	for field,offset in pairs(unique_offset) do
--- 		local uname = string.format("%s%d",field,dist_id)
--- 		local attr = lfs.attributes(string.format("./data/id_builder/%s",uname))
--- 		local fs = persistence:open("id_builder")
--- 		local save_info
--- 		if not attr then
--- 			save_info = {begin = 1,offset = id_section}
--- 			fs:save(uname,save_info)
--- 		else
--- 			save_info = fs:load(uname)
--- 			save_info.offset = id_section
--- 			save_info.begin = save_info.begin + save_info.offset
--- 			fs:save(uname,save_info)
--- 		end
-
--- 		local cursor = save_info.begin
--- 		local max = save_info.begin + save_info.offset
-
--- 		local cursor = save_info.begin
--- 		local max = save_info.begin + save_info.offset
--- 		self[string.format("alloc_%s_uid",field)] = function ()
--- 			local uid = cursor * dist_offset + dist_id
--- 			cursor = cursor + 1
--- 			if cursor >= max then
--- 				save_info.begin = cursor
--- 				max = save_info.begin + save_info.offset
--- 				fs:save(uname,save_info)
--- 			end
--- 			return uid
--- 		end
--- 	end
-
--- 	for field,offset in pairs(tmp_offset) do
--- 		local step = 1
--- 		self[string.format("alloc_%s_tid",field)] = function ()
--- 			local tid = step * dist_offset + dist_id
--- 			step = step + 1
--- 			return tid
--- 		end
--- 	end
--- end
-
-function init(self,dist_id)
-	if dist_id >= dist_offset then
-		print(string.format("error dist id:%d",dist_id))
+function init(self,serverId,distId)
+	if distId >= kPROCESS_OFFSET then
+		print(string.format("error dist id:%d",distId))
 		os.exit(1)
 	end
 
-	local db_channel = model.get_db_channel()
+	local dbChannel = model.get_db_channel()
 
-	for field,offset in pairs(unique_offset) do
-		local uname = string.format("%s%d",field,dist_id)
-		local result = db_channel:findOne("common","id_builder",{query = {id = dist_id,key = field}})
+	for field,mask in pairs(eUNIQUE_MASK) do
+		local result = dbChannel:findOne("common","id_builder",{query = {id = distId,key = field}})
 		if not result then
-			result = {begin = 1,offset = id_section}
+			result = {begin = 1,offset = kID_STEP}
 		else
 			result.begin = result.begin + result.offset
-			result.offset = id_section
+			result.offset = kID_STEP
 		end
 
 		local updator = {}
 		updator["$set"] = result
-		db_channel:update("common","id_builder",{id = dist_id,key = field},updator,true)
+		dbChannel:update("common","id_builder",{id = distId,key = field},updator,true)
 
 		local cursor = result.begin
 		local max = result.begin + result.offset
 
 		self[string.format("alloc_%s_uid",field)] = function ()
-			local uid = cursor * dist_offset + dist_id
+			local uid = cursor * kMASK_OFFSET * kSERVER_OFFSET * kPROCESS_OFFSET + mask * kSERVER_OFFSET * kPROCESS_OFFSET + serverId * kSERVER_OFFSET  + distId
 			cursor = cursor + 1
 			if cursor >= max then
 				result.begin = cursor
 				max = result.begin + result.offset
 
-				local db_channel = model.get_db_channel()
+				local dbChannel = model.get_db_channel()
 				local updator = {}
 				updator["$set"] = result
-				db_channel:update("common","id_builder",{id = dist_id,key = field},updator,true)
+				dbChannel:update("common","id_builder",{id = distId,key = field},updator,true)
 			end
 			return uid
 		end
 	end
 
-	for field,offset in pairs(tmp_offset) do
+	for field,mask in pairs(eTMP_MASK) do
+		local pool = {}
 		local step = 1
-		self[string.format("alloc_%s_tid",field)] = function ()
-			local tid = step * dist_offset + dist_id
+		local stepMax = kPROCESS_OFFSET * kSERVER_OFFSET * kMASK_OFFSET
+		self[string.format("pop_%s_tid",field)] = function ()
+			if step >= stepMax then
+				error(string.format("%s tid empty",field))
+			end
+			local tid = next(pool)
+			if tid then
+				pool[tid] = nil
+				return tid
+			end
+			local tid = step * kPROCESS_OFFSET * kMASK_OFFSET + mask * kPROCESS_OFFSET + distId
 			step = step + 1
 			return tid
+		end
+
+		self[string.format("push_%s_tid",field)] = function (tid)
+			pool[tid] = true
 		end
 	end
 end
