65585e465c83255c2fd2c2a6486fc343d556f860
2109 <cai32109@gmail.com>
2018-09-18 14:56:36 +0800

    bug

---
+++ script/module/scene/ctrl/skill_ctrl.lua
@@ -0,0 +1,21 @@
+local util = require "util"
+local vector2 = require "common.vector2"
+local object = import "module.object"
+
+cSkillCtrl = object.cObject:inherit("skillCtrl")
+
+
+function cSkillCtrl:ctor(sceneObj,skillId)
+
+end
+
+function cSkillCtrl:onCreate()
+end
+
+function cSkillCtrl:onDestroy()
+
+end
+
+function cSkillCtrl:onUpdate(now)
+
+end
