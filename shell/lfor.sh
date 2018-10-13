c8cd8551371f2e742228286a9747cfb01921c04c
mrq <362651591@qq.com>
2018-10-13 16:06:28 +0800

    update

--- shell/lfor.sh
+++ shell/lfor.sh
@@ -1,8 +1,27 @@
 
 
+#getopt
+#ab:c::(-a不接参数,-b必接参数,-c可加可不加)
+#--后的参数不通过getopt解析,也表示解析结束
+#getopt 要用$@
+#如果有混合的话要getopt -o xxx -l xxx "$@" 
+#getopt 带-o或都-l时,必须用--,指定后面为解析参数
+#getopt如果不带-o或-l时,--加上后面成为一整个参数,不参与解析
+args=`getopt -o ab:c:: -l ab,ac: -- "$@"`
+echo $args
+
+#把参数传个整个命令行参数
+set -- ${args}
 
 #等于for i in "$@"
 for i
 do
 	echo $i
 done
+
+# #把参数传给第一个命令行参数
+# set -- "${args}"
+
+
+
+
