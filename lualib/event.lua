c5752e8114b91f17b40885930c023ecba86c120d
mrq <362651591@qq.com>
2018-11-21 11:14:22 +0800

    update

--- lualib/event.lua
+++ lualib/event.lua
@@ -76,23 +76,23 @@ local function co_monitor(co,ok,state,session)
 	end
 end
 
-local function create_channel(channel_class,buffer,addr)
+local function create_channel(channel_class,channel_buff,addr)
 	local channel_obj
 	if channel_class then
-		channel_obj = channel_class:new(buffer,addr)
+		channel_obj = channel_class:new(channel_buff,addr)
 	else
 		if not _channel_base then
 			_channel_base = require "channel"
 		end
-		channel_obj = _channel_base:new(buffer,addr)
+		channel_obj = _channel_base:new(channel_buff,addr)
 	end
 	channel_obj:init()
-	_channel_ctx[buffer] = channel_obj
+	_channel_ctx[channel_buff] = channel_obj
 	return channel_obj
 end
 
 local function resolve_addr(addr)
-	local info = {}
+	local result = {}
 	local start,over = addr:find("tcp://")
 	if not start then
 		start,over = addr:find("ipc://")
@@ -100,22 +100,22 @@ local function resolve_addr(addr)
 			return
 		end
 		local file = addr:sub(over+1)
-		info.file = file
+		result.file = file
 	else
 		local ip,port = string.match(addr:sub(over+1),"(.+):(%d+)")
-		info.ip = ip
-		info.port = port
+		result.ip = ip
+		result.port = port
 	end
-	return info
+	return result
 end
 
 function _M.listen(addr,header,callback,channel_class,multi)
 	local listener
-	local info = resolve_addr(addr)
-	if not info then
+	local addr_form = resolve_addr(addr)
+	if not addr_form then
 		return false,string.format("error addr:%s",addr)
 	end
-	local listener,reason = _event:listen(header,multi or false,info)
+	local listener,reason = _event:listen(header,multi or false,addr_form)
 
 	if not listener then
 		return false,reason
@@ -125,48 +125,40 @@ function _M.listen(addr,header,callback,channel_class,multi)
 end
 
 function _M.connect(addr,header,sync,channel_class)
-	local info = resolve_addr(addr)
-	if not info then
+	local addr_form = resolve_addr(addr)
+	if not addr_form then
 		return false,string.format("error addr:%s",addr)
 	end
+	
 	if sync then
-		local buffer,reason
-		if info.file then
-			buffer,reason = _event:connect(header,0,info)
-		else
-			buffer,reason = _event:connect(header,0,info)
-		end
-
-		if not buffer then
+		local channel_buff,reason = _event:connect(header,0,addr_form)
+		if not channel_buff then
 			return false,reason
 		end
-		return create_channel(channel_class,buffer,info.file or string.format("%s:%s",info.ip,info.port))
-	else
-		local session = _M.gen_session()
-		local co = co_running()
-		assert(co ~= _main_co,string.format("cannot async connect in main co"))
-
-		local ok,reason
-		if info.file then
-			ok,reason = _event:connect(header,session,info)
-		else
-			ok,reason = _event:connect(header,session,info)
-		end
-		if not ok then
-			return false,reason
-		end
-		local ok,buffer = _M.wait(session)
-		if ok then
-			return create_channel(channel_class,buffer,info.file or string.format("%s:%s",info.ip,info.port))
-		else
-			return false,buffer
-		end
+		return create_channel(channel_class,channel_buff,addr)
 	end
+
+	if co_running() ~= _main_co then
+		error("cannot async connect in main co")
+	end
+
+	local session = _M.gen_session()
+
+	local ok,reason  = _event:connect(header,session,addr_form)
+	if not ok then
+		return false,reason
+	end
+	local ok,channel_buff = _M.wait(session)
+	if not ok then
+		return ok,channel_buff
+	end
+
+	return create_channel(channel_class,channel_buff,addr)
 end
 
 function _M.bind(fd,channel_class)
-	local buffer = _event:bind(fd)
-	return create_channel(channel_class,buffer)
+	local channel_buff = _event:bind(fd)
+	return create_channel(channel_class,channel_buff)
 end
 
 function _M.sleep(ti)
@@ -311,9 +303,9 @@ function _M.dispatch()
 		end
 	end
 
-	for buffer in pairs(_channel_ctx) do
-		if buffer:alive() then
-			buffer:close(true)
+	for channel_buff in pairs(_channel_ctx) do
+		if channel_buff:alive() then
+			channel_buff:close(true)
 		end
 	end
 
@@ -363,9 +355,9 @@ EV[EV_TIMEOUT] = function (timer)
 	end
 end
 
-EV[EV_ACCEPT] = function (listener,buffer,addr)
+EV[EV_ACCEPT] = function (listener,channel_buff,addr)
 	local info = _listener_ctx[listener]
-	local channel_obj = create_channel(info.channel_class,buffer,addr)
+	local channel_obj = create_channel(info.channel_class,channel_buff,addr)
 	info.callback(listener,channel_obj)
 end
 
@@ -373,13 +365,13 @@ EV[EV_CONNECT] = function (...)
 	_M.wakeup(...)
 end
 
-EV[EV_DATA] = function (buffer,data,size)
-	local channel = _channel_ctx[buffer]
+EV[EV_DATA] = function (channel_buff,data,size)
+	local channel = _channel_ctx[channel_buff]
 	channel:data(data,size)
 end
 
-EV[EV_ERROR] = function (buffer)
-	local channel = _channel_ctx[buffer]
+EV[EV_ERROR] = function (channel_buff)
+	local channel = _channel_ctx[channel_buff]
 	channel:disconnect()
 end
 
