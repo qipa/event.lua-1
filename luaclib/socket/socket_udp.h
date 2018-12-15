43da177739fc4ac7350f66b2187bce92bd48cc31
2109 <cai32109@gmail.com>
2018-12-15 09:57:35 +0800

    bug

--- luaclib/socket/socket_udp.h
+++ luaclib/socket/socket_udp.h
@@ -9,6 +9,6 @@ typedef void (*udp_session_event_callback)(struct udp_session*,void *userdata);
 struct udp_session* udp_session_new(struct ev_loop_ctx* loop_ctx, size_t recv_size);
 void udp_session_destroy(struct udp_session* session);
 struct udp_session* udp_session_bind(struct ev_loop_ctx* loop_ctx, const char* ip,ushort port,size_t recv_size);
-
 int udp_session_write(struct udp_session* session, char* data, size_t size, const char* ip, ushort port);
+void udp_session_setcb(struct udp_session* session, udp_session_read_callback read_cb, udp_session_event_callback event_cb, void* userdata);
 #endif
