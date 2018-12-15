cc229ad77e11b956ae140bde5343f767ef9c4bde
mrq <mrq@localhost.localdomain>
2018-12-14 20:45:43 -0500

    update

--- luaclib/lua-ev.c
+++ luaclib/lua-ev.c
@@ -915,7 +915,7 @@ _udp_new(lua_State* L) {
 	ludp_session->callback = callback;
 	ludp_session->ref = meta_init(L,META_UDP);
 	
-	ludp_session_setcb(ludp_session->session, udp_recv, NULL);
+	udp_session_setcb(ludp_session->session, udp_recv, NULL);
 
 	return 1;
 }
