23572715ca0bd8587794e54ad105b1ca042dc9c1
mrq <362651591@qq.com>
2018-12-20 10:05:28 +0800

    update

--- shell/vim_install.sh
+++ shell/vim_install.sh
@@ -15,21 +15,33 @@ fi
 
 if [ ! -d ./the_silver_searcher ];then
 	git clone https://github.com/ggreer/the_silver_searcher.git
+	cd the_silver_searcher
+	./autogen.sh && ./configure && make
+	sudo make install
+	cd ..
+	
 fi
 
 if [ ! -d ./ctags ];then 
 	git clone https://github.com/universal-ctags/ctags.git
+	cd ctags
+	./autogen.sh && ./configure && make
+	sudo make install
+	cd ..
 fi
 
 if [ ! -f ./Python-2.7.5.tgz ];then
 	wget https://www.python.org/ftp/python/2.7.5/Python-2.7.5.tgz
 	tar zxvf Python-2.7.5.tgz
 	cd Python-2.7.5 && ./configure --enable-shared && make
+	sudo make install
+	cd ..
 fi
 
-cd ~/vim_asset/vim
-./configure  --enable-pythoninterp=yes --with-python-config-dir=/usr/lib/python2.7/config
+cd vim
+./configure  --enable-pythoninterp=yes --with-python-config-dir=/usr/lib64/python2.7/config
 sudo make
+sudo make install
 sudo cp /usr/local/bin/vim /usr/bin/vim
 
 if [ ! -d ~/.vim ];then
